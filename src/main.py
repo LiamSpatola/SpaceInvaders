004e2f445c3bc7bbb2eb12631d26b8a52db2fec9
SPAT01 <SPAT01@stpiusx.nsw.edu.au>
2024-05-25 13:28:08 +1000

    Changed power up mechanics
    Ensured power ups would not spawn off the screen, and changed the bonus health power up to ensure the player could not get above 3 lives.

--- src/main.py
+++ src/main.py
@@ -286,7 +286,7 @@ def main():
                 case "extra_firepower":
                     img = EXTRA_FIREPOWER_POWER_UP
             
-            power_up = PowerUp(random.randrange(0, WIDTH + 50), random.randrange(500, HEIGHT - 150), img, random.randrange(200, 600), variant)
+            power_up = PowerUp(random.randrange(0, WIDTH + 100), random.randrange(500, HEIGHT - 150), img, random.randrange(200, 600), variant)
             power_ups.append(power_up)
 
         # Updating the powerups, checking for collision, and running the countdown until their destruction
@@ -303,7 +303,7 @@ def main():
                         case "bonus_health":
                             if player_ship.health < 100:
                                 player_ship.health += 10
-                            else:
+                            elif player_ship.health < 100 and lives < 3:
                                 lives += 1
                                 player_ship.health = 10
                         case "extra_firepower":
